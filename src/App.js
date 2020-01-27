9ef6fd7e88495f34c2cd3476dee1e85844d73fc9
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-27 17:35:02 -0500

    created a post form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+
 import Posts from './Components/Posts'
+import PostForm from './Components/Postform';
 
 function App() {
   return (
     <div className="App">
+      <PostForm></PostForm>
+      <hr></hr>
       <Posts></Posts>
     </div>
   );

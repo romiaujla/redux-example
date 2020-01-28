c7b14f2b22b430b693116dc482144ddce21c4177
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-28 18:05:29 -0500

    imported Provider and initialized store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Posts from './Components/Posts'
 import PostForm from './Components/Postform';
 
-function App() {
-  return (
-    <div className="App">
-      <PostForm></PostForm>
-      <hr></hr>
-      <Posts></Posts>
-    </div>
-  );
+const store = createStore(() => [], {} , applyMiddleware())
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <PostForm></PostForm>
+          <hr></hr>
+          <Posts></Posts>
+        </div>
+      </Provider>
+    );  
+  }
 }
 
 export default App;
